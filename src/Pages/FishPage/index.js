1634887c7277065d8ee6804071d4227808bd5784
huanglifeng <1075306513@qq.com>
2018-11-09 18:05:53 +0800

    新增首页聊天窗口组件

--- src/Pages/FishPage/index.js
+++ src/Pages/FishPage/index.js
@@ -6,7 +6,7 @@ import ReactPlayer from 'react-player';
 import "./index.css";
 
 let pageData;
-const tmpData=[
+const tmpData = [
   {
     "completed": true,
     "order": 0,
@@ -117,43 +117,37 @@ class Fish extends Component {
       isLoading: true,
       segIndex: 0,
       pageData: '',
-      imgHeight: window.innerHeight*0.8,
-      fishData:[]
+      imgHeight: window.innerHeight * 0.8,
+      fishData: []
     };
   }
   componentWillMount() {
-    let fishData=[];
-    fetch(window.API_CONFIG.API_PATH+'/fishdatas', {
-        method: 'GET'
-    }).then(res=>res.json()).then(
-      (result)=>{
-        if(result&&result.length>0)
-        {
-          let item=result.filter((e)=>{
-            if(new Date(e.date).toLocaleDateString()===new Date().toLocaleDateString())
-            {
+    let fishData = [];
+    fetch(window.API_CONFIG.API_PATH + '/fishdatas', {
+      method: 'GET'
+    }).then(res => res.json()).then(
+      (result) => {
+        if (result && result.length > 0) {
+          let item = result.filter((e) => {
+            if (new Date(e.date).toLocaleDateString() === new Date().toLocaleDateString()) {
               return e;
             }
           })[0];
-          let item1=result.filter((e)=>{
-            if(new Date(e.date).toLocaleDateString()===new Date(new Date().getTime()-24*60*60*1000).toLocaleDateString())
-            {
+          let item1 = result.filter((e) => {
+            if (new Date(e.date).toLocaleDateString() === new Date(new Date().getTime() - 24 * 60 * 60 * 1000).toLocaleDateString()) {
               return e;
             }
           })[0];
-          if(!item)
-          {
-            item=tmpData[0];
+          if (!item) {
+            item = tmpData[0];
           }
-          fishData.push({"date":item.date,"data":item.data,"noticeContent":item.noticeContent})
-          if(!item1)
-          {
-            item1=tmpData[1];
+          fishData.push({ "date": item.date, "data": item.data, "noticeContent": item.noticeContent })
+          if (!item1) {
+            item1 = tmpData[1];
           }
-          fishData.push({"date":item1.date,"data":item1.data,"noticeContent":item1.noticeContent})
-          if(fishData.length>0)
-          {
-            this.setState({fishData})
+          fishData.push({ "date": item1.date, "data": item1.data, "noticeContent": item1.noticeContent })
+          if (fishData.length > 0) {
+            this.setState({ fishData })
           }
         }
       }
@@ -161,80 +155,146 @@ class Fish extends Component {
   }
   onSegChange = (e) => {
     console.log(`selectedIndex:${e.nativeEvent.selectedSegmentIndex}`);
-    const segIndex = e.nativeEvent.selectedSegmentIndex; 
-    this.setState({segIndex})
+    const segIndex = e.nativeEvent.selectedSegmentIndex;
+    this.setState({ segIndex })
   }
   render() {
-    const {segIndex, imgHeight, fishData} = this.state;
-    pageData = segIndex===0?fishData[0]:fishData[1];
+    const { segIndex, imgHeight, fishData } = this.state;
+    pageData = segIndex === 0 ? fishData[0] : fishData[1];
     return (
       <WingBlank size="lg" className="container">
-        <SegmentedControl values={['今日','昨日']} onChange={this.onSegChange}/>
+        <SegmentedControl values={['今日', '昨日']} onChange={this.onSegChange} />
         <WhiteSpace size="lg" />
-        <NoticeBar marqueeProps={{loop: true, style: {padding: '0 7.5px'}}}>
-            {pageData&&pageData.data?pageData.noticeContent:null}
+        <NoticeBar marqueeProps={{ loop: true, style: { padding: '0 7.5px' } }}>
+          {pageData && pageData.data ? pageData.noticeContent : null}
         </NoticeBar>
         <WhiteSpace size="lg" />
-        <Carousel
-          autoplay={false}
-          animation={true}
-          //infinite
-          //beforeChange={(from, to) => console.log(`slide from ${from} to ${to}`)}
-          //afterChange={index => console.log('slide to', index)}
-        >
-          {pageData&&pageData.data?pageData.data.map(val => (
-            <a
-              key={val}
-              //href=""
-              style={{ display: 'inline-block', width: '100%', height: imgHeight }}
+        {
+          segIndex === 0 ?
+            <Carousel
+              autoplay={false}
+              animation={true}
+              key={1}
+            //infinite
+            //beforeChange={(from, to) => console.log(`slide from ${from} to ${to}`)}
+            //afterChange={index => console.log('slide to', index)}
             >
-              {
-                val.type==='img' ?
-                <img
-                src={val.src}
-                alt=""
-                style={{ width: '100%', minHeight:512, verticalAlign: 'top' }}
-                onLoad={() => {
-                  window.dispatchEvent(new Event('resize'));
-                }}
-              />
-              :
-              <div className='player-wrapper'>
-                <ReactPlayer
-                  playing={false}
-                  controls={true}
-                  loop
-                  className='react-player'
-                  url={val.src}
-                  width='100%'
-                  height={imgHeight}
-                  onReady={() => {
-                    window.dispatchEvent(new Event('resize'));
-                  }}
-                />
-              </div>
-                // <ReactPlayer url={val.src} playing />
-                // <video 
-                // controls 
-                // muted 
-                // webkit-playsinline="true" 
-                // playsinline="true" 
-                // //poster="" 
-                // src={val.src}
-                // style={{ width: '100%', height: imgHeight, verticalAlign: 'top', minHeight: 512 }}
-                // onLoad={() => {
-                //   window.dispatchEvent(new Event('resize'));
-                // }}
-                // >
-                //   您的浏览器不支持该视频播放
-                // </video>
+              {pageData && pageData.data ? pageData.data.map(val => (
+                <a
+                  key={val}
+                  //href=""
+                  style={{ display: 'inline-block', width: '100%', height: imgHeight }}
+                >
+                  {
+                    val.type === 'img' ?
+                      <img
+                        src={val.src}
+                        alt=""
+                        style={{ width: '100%', minHeight: 512, verticalAlign: 'top' }}
+                        onLoad={() => {
+                          window.dispatchEvent(new Event('resize'));
+                        }}
+                      />
+                      :
+                      <div className='player-wrapper'>
+                        <ReactPlayer
+                          playing={false}
+                          controls={true}
+                          loop
+                          className='react-player'
+                          url={val.src}
+                          width='100%'
+                          height={imgHeight}
+                          onReady={() => {
+                            window.dispatchEvent(new Event('resize'));
+                          }}
+                        />
+                      </div>
+                    // <ReactPlayer url={val.src} playing />
+                    // <video 
+                    // controls 
+                    // muted 
+                    // webkit-playsinline="true" 
+                    // playsinline="true" 
+                    // //poster="" 
+                    // src={val.src}
+                    // style={{ width: '100%', height: imgHeight, verticalAlign: 'top', minHeight: 512 }}
+                    // onLoad={() => {
+                    //   window.dispatchEvent(new Event('resize'));
+                    // }}
+                    // >
+                    //   您的浏览器不支持该视频播放
+                    // </video>
+                  }
+                </a>
+              ))
+                :
+                null
               }
-            </a>
-          ))
-        :
-        null
+            </Carousel>
+            :
+            <Carousel
+              autoplay={false}
+              animation={true}
+              key={2}
+            //infinite
+            //beforeChange={(from, to) => console.log(`slide from ${from} to ${to}`)}
+            //afterChange={index => console.log('slide to', index)}
+            >
+              {pageData && pageData.data ? pageData.data.map(val => (
+                <a
+                  key={val}
+                  //href=""
+                  style={{ display: 'inline-block', width: '100%', height: imgHeight }}
+                >
+                  {
+                    val.type === 'img' ?
+                      <img
+                        src={val.src}
+                        alt=""
+                        style={{ width: '100%', minHeight: 512, verticalAlign: 'top' }}
+                        onLoad={() => {
+                          window.dispatchEvent(new Event('resize'));
+                        }}
+                      />
+                      :
+                      <div className='player-wrapper'>
+                        <ReactPlayer
+                          playing={false}
+                          controls={true}
+                          loop
+                          className='react-player'
+                          url={val.src}
+                          width='100%'
+                          height={imgHeight}
+                          onReady={() => {
+                            window.dispatchEvent(new Event('resize'));
+                          }}
+                        />
+                      </div>
+                    // <ReactPlayer url={val.src} playing />
+                    // <video 
+                    // controls 
+                    // muted 
+                    // webkit-playsinline="true" 
+                    // playsinline="true" 
+                    // //poster="" 
+                    // src={val.src}
+                    // style={{ width: '100%', height: imgHeight, verticalAlign: 'top', minHeight: 512 }}
+                    // onLoad={() => {
+                    //   window.dispatchEvent(new Event('resize'));
+                    // }}
+                    // >
+                    //   您的浏览器不支持该视频播放
+                    // </video>
+                  }
+                </a>
+              ))
+                :
+                null
+              }
+            </Carousel>
         }
-        </Carousel>
       </WingBlank>
     );
   }
